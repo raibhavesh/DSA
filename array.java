4b3fc5c41ec1ef0ad3ed521e77271e6eb8ddf597
raibhavesh <113577193+raibhavesh@users.noreply.github.com>
2024-08-03 23:38:45 +0530

    arrays1

--- array.java
+++ array.java
@@ -1,4 +1,11 @@
-//hello
-//lol
-//hi guys my name is bhavesh
+import java.util.*;
+public class array{
+    public static void main(String args[]){
+        int[] marks = new int[3];
+        marks[0]=97;
+        marks[1]=98;
+        marks[2]=100;
+        System.out.println(marks[1]);
+    }
+}
 
