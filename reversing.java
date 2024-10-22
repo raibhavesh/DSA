b650e51f32f296b20099764b89936baecf579949
raibhavesh <113577193+raibhavesh@users.noreply.github.com>
2024-10-23 00:28:53 +0530

    pending

---
+++ reversing.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class reversing {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int fig = sc.nextInt();
+        int[] number=new int[fig];
+        int[] temp =new int[fig];
+
+        for(int i=0;i<fig;i++){
+            number[i]=sc.nextInt();
+            
+        
+
+        
+}
+}
