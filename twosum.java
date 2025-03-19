71f89484136b3ad7dde400585db3ed121e66be5e
raibhavesh <113577193+raibhavesh@users.noreply.github.com>
2025-03-19 10:26:49 +0530

    sum of 2 indices

---
+++ twosum.java
@@ -0,0 +1,24 @@
+public class twosum{
+    public static String TwoSum(int n, int arr[], int target){
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(arr[i]+arr[j]==target){
+                    return "Yes, The indices are: " + i + " " + j;
+
+                }
+
+            }
+            
+        }
+        return "No";
+
+    }
+    public static void main(String[] args){
+        int arr[]={2,7,11,15};
+        int n = arr.length;
+        int target = 9;
+        String result = TwoSum(n,arr,target);
+        System.out.println(result);
+        
+    }
+}
