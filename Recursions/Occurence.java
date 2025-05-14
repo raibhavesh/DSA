7f0b18186ba6dd81eadb5ad8eb8a5868773857fd
raibhavesh <113577193+raibhavesh@users.noreply.github.com>
2025-05-14 18:22:37 +0530

    finding occurence os an element

---
+++ Recursions/Occurence.java
@@ -0,0 +1,29 @@
+public class Occurence {
+    public static int first = -1;
+    public static int last = -1;
+
+    public static void findOccurence(String str, int idx, char element){
+        if(idx == str.length()){
+            System.out.println(first);
+            System.out.println(last);
+            return ;
+        }
+        char currchar = str.charAt(idx);
+        if(currchar == element){
+            if(first == -1){
+                first = idx;
+            }
+            else{
+                last = idx;
+            }
+        }
+        findOccurence(str, idx+1, element);
+
+        
+    }
+
+    public static void main(String args[]){
+        String str = "baaaaabcdefgaadea";
+        findOccurence(str, 0 , 'a');
+    }
+}
